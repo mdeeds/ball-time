d3ed67b755a7fc079b19e9e2e70ab4b5116a7292
Matt Deeds <mdeeds@gmail.com>
2023-01-22 14:16:37 -0800

    Add LFO to reward sound

--- ts/sfx/rewardSound.ts
+++ ts/sfx/rewardSound.ts
@@ -2,15 +2,20 @@ export class RewardSound {
   private context: AudioContext;
   private oscillator1: OscillatorNode;
   private oscillator2: OscillatorNode;
+  private lfo: OscillatorNode;
   private filter: BiquadFilterNode;
   private gain: GainNode;
+  private detunAmt: GainNode;
 
   constructor() {
     this.context = new AudioContext();
     this.oscillator1 = this.context.createOscillator();
     this.oscillator2 = this.context.createOscillator();
+    this.lfo = this.context.createOscillator();
     this.filter = this.context.createBiquadFilter();
     this.gain = this.context.createGain();
+    this.detunAmt = this.context.createGain();
+    this.detunAmt.gain.value = 50;
   }
 
   public start(): void {
@@ -18,11 +23,16 @@ export class RewardSound {
     this.oscillator1.type = 'sine';
     this.oscillator2.type = 'triangle';
     this.gain.gain.value = 0.3;
+    this.lfo.type = 'sine';
 
     // Set the frequency of the oscillators to different values
     this.oscillator1.frequency.value = 880;
     this.oscillator2.frequency.value =
       this.oscillator1.frequency.value * 5 / 2;
+    this.lfo.frequency.value = 7;
+    this.lfo.connect(this.detunAmt);
+    this.detunAmt.connect(this.oscillator1.detune);
+    this.detunAmt.connect(this.oscillator2.detune);
 
     // Start the oscillators
     this.oscillator1.start();
